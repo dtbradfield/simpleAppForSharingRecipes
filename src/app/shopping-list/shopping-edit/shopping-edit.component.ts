19a7800c0b6043d81aaaf77476dfb9b29dc39f2c
DonTheChimera <donaldtbradfield@gmail.com>
2019-02-21 09:27:27 -0600

    reset form after add/update

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -42,6 +42,8 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
     } else {
         this.slService.addIngredient(newIngredient);
     }
+    this.editMode = false;
+     form.reset();
   }
 
   ngOnDestroy() {
